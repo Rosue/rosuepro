2e7ae1ecf76526481c979a7549a5e57814866b12
Rosue Walford <rosuepro@gmail.com>
2025-03-27 07:44:08 -0500

    Added Portfolio and Resume

--- src/pages/Portforlio.js
+++ src/pages/Portforlio.js
@@ -1,8 +1,43 @@
 import React from 'react'
 import TopNavbar from '../components/TopNavbar'
+import { Container } from 'react-bootstrap'
+import Footer from '../components/Footer'
 
 export default function Portforlio() {
   return (
-    <TopNavbar/>
+    <>
+      <TopNavbar/>
+
+      <Container className="main-content">
+
+        <h1 className="text-center mt-5  txt-cool">Portfolio</h1>
+        <p className="text-center"><sm className="txt-cool mb-5">A showcase of some of our work</sm></p>
+        <div className="card mb-5 ">
+          <img src="reggaewheels-2482a.web.app_ (1).png" className="card-img-top" alt="Reggae Wheels Website Image"/>
+          <div className="card-body">
+            <h5 className="card-title">Reggae Wheels Website</h5>
+            <p className="card-text">This website books and advertises tours, trips and  rentals</p>
+          </div>
+        </div>
+
+        <div className="card mb-3 mt-5 ">
+          <img src="rj-detailing.web.app_calendar.png" className="card-img-top" alt="Reggae Wheels Website Image"/>
+          <div className="card-body">
+            <h5 className="card-title">Shynz By Onyx Website</h5>
+            <p className="card-text">This is an appointment website where users can set and see the schedule</p>
+          </div>
+        </div>
+
+        <div className="card mb-3 mt-5 ">
+          <img src="432952191_923514793107512_60065450142670162_ysn.jpg" className="card-img-top" alt="Reggae Wheels Website Image"/>
+          <div className="card-body">
+            <h5 className="card-title">Yahsonice Website</h5>
+            <p className="card-text">This website connects Bar Owners with Bartenders</p>
+          </div>
+        </div>
+      </Container>
+
+      <Footer />
+    </>
   )
 }
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,8 +1,69 @@
 import React from 'react'
 import TopNavbar from '../components/TopNavbar'
+import { Accordion, Container } from 'react-bootstrap'
+import Footer from '../components/Footer'
 
 export default function Resume() {
   return (
+    <>
     <TopNavbar/>
+    <Container className="main-content">
+    <h1 className="text-center mt-5  mb-5 txt-cool">Resume</h1>
+
+      <Accordion defaultActiveKey="0" className='mb-5'>
+      <Accordion.Item eventKey="0">
+          <Accordion.Header>Introduction</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>Skills</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="2">
+          <Accordion.Header>Experience</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="3">
+          <Accordion.Header>Education</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+
+
+    </Container>
+    <Footer />
+    </>
   )
 }
