b3c7b69cb4b82351f6dc0a0f9ca40cafd44f1fbc
Rosue Walford <rosuepro@gmail.com>
2025-04-18 17:11:55 -0500

    Img alt updated

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -3,7 +3,6 @@ import TopNavbar from '../components/TopNavbar'
 import { Container } from 'react-bootstrap'
 import Footer from '../components/Footer'
 import { Link } from 'react-router-dom'
-import BasicTipsMarketMusic from './blog/BasicTipsMarketMusic'
 
 export default function Blog() {
   return (
@@ -12,7 +11,7 @@ export default function Blog() {
     <Container className='main-content'>
       <h1 className='text-center txt-cool'>Blog</h1>
     <div className="card mt-5 mb-5" style={{"width": "18rem"}}>
-      <img src="headphones-4595492_1280.jpg" className="card-img-top" alt="Basic Tips on how to Market your music image"/>
+      <img src="headphones-4595492_1280.jpg" className="card-img-top" alt="Market_your_music"/>
       <div className="card-body">
         <h5 className="card-title">Basic Tips on how to Market your music</h5>
         <p className="card-text">In the fast paced and constantly evolving music world we currently live in, and one which boasts of thousands and thousands of music artistes, the success of nowadays musicians especially the upcoming ones is almost not guaranteed unless some pertinent things are put into consideration. In this article, I have decided to put together a few tips for upcoming musicians like you that will definitely help your career push.</p>
